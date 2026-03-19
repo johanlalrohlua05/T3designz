c1896b3765b79f858f7a7d5336d3f908278057f4
T3designz <johanlalrohlua05@gmail.com>
2026-03-19 19:54:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let products = JSON.parse(localStorage.getItem("products")) || [
-    { id: 1, name: "T-Shirt", price: 850, image: "C:\Users\MY PC\OneDrive\Desktop\image\tshirt.jpg" },
-    { id: 2, name: "Hoodie", price: 1200, image: "C:\Users\MY PC\OneDrive\Desktop\image\hoodie.jpg" }
+    { id: 1, name: "T-Shirt", price: 850, image: "C:\Users\MY PC\OneDrive\Desktop\New folder\image\tshirt.jpg" },
+    { id: 2, name: "Hoodie", price: 1200, image: "C:\Users\MY PC\OneDrive\Desktop\New folder\image\hoodie.jpg" }
 ];
 
 let cart = JSON.parse(localStorage.getItem("cart")) || {};
